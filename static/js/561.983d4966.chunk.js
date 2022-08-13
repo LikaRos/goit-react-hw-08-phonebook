fe7de13d376ea40a0ea7b4cf17b49664071f2ae6
LikaRos <42115401+LikaRos@users.noreply.github.com>
2022-08-13 05:45:25 +0000

    Deploying to gh-pages from @ LikaRos/goit-react-hw-08-phonebook@ed4861f181c8d133c8d3e6e7248d12cb7c2c9661 🚀

---
+++ static/js/561.983d4966.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[561],{6561:function(e,t,r){r.r(t),r.d(t,{HomeView:function(){return c}});var n=r(184),c=function(){return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("h1",{children:" welcome to CONTACTS"}),";"]})};t.default=c}}]);
+//# sourceMappingURL=561.983d4966.chunk.js.map
